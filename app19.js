44300f30967f034c42e5ec98c516085c8269999f
Felipe <afmejia23@utp.edu.co>
2018-01-09 11:59:20 +0200

    Using directives

--- app19.js
+++ app19.js
@@ -1,16 +1,16 @@
 angular.module('functionalities', [])
 .directive("entering", function(){
- return function(scope, element) {
+ return function(scope, element, attrs) {
       element.bind("mouseenter", function(){
-
+        element.addClass(attrs.entering);
       })
     }
 })
 
 .directive("leaving", function(){
- return function(scope, element) {
+ return function(scope, element, attrs) {
       element.bind("mouseleave", function(){
-
+        element.removeClass(attrs.entering);
       })
     }
 });
